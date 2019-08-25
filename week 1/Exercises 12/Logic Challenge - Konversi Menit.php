67189c4a421fd2b0a41949881e0ad6830490dde2
kevadamar <54131040+kevadamar@users.noreply.github.com>
2019-08-25 18:52:50 +0700

    update menit rev.1

--- week 1/Exercises 12/Logic Challenge - Konversi Menit.php
+++ week 1/Exercises 12/Logic Challenge - Konversi Menit.php
@@ -1,39 +1,18 @@
 <?php
+function konversiMenit($menit) {
+    
+    //menggunakan built-in function floor()
+    $jam = floor($menit / 60);
+    $mnt = $menit % 60;
 
-function konversiMenit($menit){
-    if($menit>0 && $menit<60){
-        $lama="0 : ".$menit."\n";
-        return $lama;
-    }
-    else if($menit>=60 && $menit<70){
-        $detik=$menit%60;
-        $mnt=$menit-$detik;
-        $mnt=$mnt/60;
-        $lama=$mnt." : 0".$detik."\n";
-        return $lama;
-    }
-    else if($menit >=70 && $menit <=88 ){
+    return $mnt <= 4 ? $jam.":0".$mnt."\n" : $jam .':' .$mnt."\n";
+ }
 
-        $detik=$menit%60;
-        $mnt=$menit-$detik;
-        $mnt=$mnt/60;
-        $lama=$mnt." : ".$detik."\n";
-        return $lama;
-    }
-
-    else if ( $menit >=120|| $menit <130){
-      
-        $detik=$menit%60;
-        $mnt=$menit-$detik;
-        $mnt=$mnt/60;
-        $lama=$mnt." : 0".$detik."\n";
-        return $lama;
-
-    }
-}
+// TEST CASES
 echo konversiMenit(63); // 1:03
 echo konversiMenit(124); // 2:04
 echo konversiMenit(53); // 0:53
 echo konversiMenit(88); // 1:28
 echo konversiMenit(120); // 2:00
+
 ?>
