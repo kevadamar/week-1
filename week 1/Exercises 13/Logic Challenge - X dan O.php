e2fbd14317703a1b1d000c4e3e9859e57981d2ae
kevadamar <54131040+kevadamar@users.noreply.github.com>
2019-08-25 16:24:11 +0700

    update - week 1 rev.1

--- week 1/Exercises 13/Logic Challenge - X dan O.php
+++ week 1/Exercises 13/Logic Challenge - X dan O.php
@@ -1,18 +1,23 @@
 <?php
 function xo($str) {
-    
+    // echo $str[2];
     $x = 0;
     $o = 0;
     $i = 0; //iterasi awal
     for (; $i <= strlen($str); $i++) {
         if ($str[$i] === "x") {
+
+          return "true\n";
             $i++;
         } else if ($str[$i] === "o") {
+
+          return "false\n";
             $o++;
         }
         //ternary operator
-        return $x === $o ? "true\n" : "false\n";
+        // return $x === $o ? "true\n" : "false\n";
     }
+    
 }
 
 // TEST CASES
@@ -21,4 +26,5 @@ echo xo("oxooxo"); // false
 echo xo("oxo"); // false
 echo xo("xxxooo"); // true
 echo xo("xoxooxxo"); // true
+
 ?>
